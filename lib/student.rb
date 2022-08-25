ffd304f3915553c26b462bb150ad02ea3b329146
Olga Nadongo <winnienadongo@gmail.com>
2022-08-25 17:21:08 +0300

    commit tree f36f2dd26a39c5bdb0b881d9b8dc70d3660ec394

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
 
+require_relative "./user.rb"
+
+class Student <User
+
+def initialize
+    @knowledge = []
+end
+     
+def learn (learnt_knowledge)
+  @knowledge <<learnt_knowledge
+end
+ def knowledge
+    @knowledge
+ end
 end
